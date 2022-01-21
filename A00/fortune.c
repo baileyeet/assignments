268f6b35657329236f82932380dca071c886a7e7
bailey_machiko <baileyhirota@icloud.com>
2022-01-21 13:26:12 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c
+ * Author: Bailey Hirota
+ * Date: 1/21/2022
+ * Print fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("You will be rich.\n");
+  return 0;
+}
