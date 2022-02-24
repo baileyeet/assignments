337d3679f5ed27f82396b1a63f022384402d6e2d
Bailey Machiko Hirota <bhirota@goldengate.cs.brynmawr.edu>
2022-02-24 15:34:27 -0500

    Jam 2

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -2,30 +2,52 @@
 #include <string.h>
 
 struct cake {
-  float cost;
-  char flavor[16];
-  struct cake* next;
+	float cost;
+	char flavor[16];
+	struct cake* next;
 };
 
 struct cake makeCake(const char* flavor, float cost) {
-  struct cake newCake;
-  strncpy(newCake.flavor, flavor, 16);
-  newCake.cost = cost;
-  newCake.next = NULL;
-  return newCake;
+	struct cake newCake;
+	strncpy(newCake.flavor, flavor, 16);
+	newCake.cost = cost;
+	newCake.next = NULL;
+	return newCake;
 }
 
 // todo: implement cheapestCake
+struct cheapestCake(struct cake head) {
+	struct cake temp = &head;
+	//float least = head.cost;
+	//char least_name* = head.name;
+	while (temp.next != NULL) {
+	
+		//while (temp.next != NULL) {
+		printf("cake: %S ($%f)", temp.flavor, temp.cost);
+		if (temp.next.cost < temp.cost) {
+			temp = temp.next;
+			//least_name = temp.next.name;
+		} 
+		temp = temp.next;	
+		//}
+	}
+	return temp;
+}
 
 int main() {
-  struct cake cake1 = makeCake("red velvet", 2.00);
-  struct cake cake2 = makeCake("chocolate", 1.75);
-  struct cake cake3 = makeCake("mocha", 3.50);
-  // todo: define 4th cake
+	struct cake cake1 = makeCake("red velvet", 2.00);
+	struct cake cake2 = makeCake("chocolate", 1.75);
+	struct cake cake3 = makeCake("mocha", 3.50);
+	struct cake cake4 = makeCake("vanilla", 5.50);
+	// todo: define 4th cake
 
-  cake1.next = &cake2;
-  cake2.next = &cake3;
-  // draw stack and heap here
+	cake1.next = &cake2;
+	cake2.next = &cake3;
+	cake3.next = &cake4;
+	// draw stack and heap here
 
-  // todo: call cheapestCake
+	printf("%s ", cake1.flavor);
+	// todo: call cheapestCak
+
+	printf("The cheapest cake is %s", cheapestCake(cake1).name);
 }
--- J02/readvector.c
+++ J02/readvector.c
@@ -4,23 +4,43 @@
 // readvector opens and reads the given file
 // The size of the vector should be stored in the parameter size
 // The values should be returned in a flat float array
-float* readvector(const char* filename, int *size) {
-  // todo: implement me
-  return NULL;
+struct vector {
+	double one;
+	//	double two;
+	//	double three;
+
+};
+
+void readvector(const char* filename, int *size) {
+	FILE *fp;
+	fp = fopen(filename,"r");
+	struct vector* v;
+	//	v = malloc(sizeof(struct vector));
+	//char current_word[200];
+	if(fp == NULL) { // check if successful
+		perror("Error in opening file");
+		exit(1);
+	}
+	fscanf(fp, "%d", size);
+	v = malloc(sizeof(struct vector));
+	while (*size > 0) {
+		fscanf(fp, "%lf", &v->one);
+		 printf("%f\n", v->one);
+		*size = *size - 1;
+	}
+	free(v);
+	v = NULL;	
 }
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: readvector <filename>\n");
-    exit(0);
-  }
+	if (argc != 2) {
+		printf("usage: readvector <filename>\n");
+		exit(0);
+	}
 
-  int size = 0;
-  float* vector = readvector(argv[1], &size);
-
-  // todo: print vector values
-
-  free(vector);
-  return 0;
+	int size = 0;
+	readvector(argv[1], &size);
+	// todo: print vector values
+	return 0;
 }
 
