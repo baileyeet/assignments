91eb1131b635775e275c1e675b71e2228d47c347
Bailey Machiko Hirota <bhirota@goldengate.cs.brynmawr.edu>
2022-03-05 11:48:09 -0500

    updates

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
 int main(int argc, char** argv) {
-  return 0;
+	int w = 0;
+	int h = 0;
+	int count = 0;
+	unsigned char str = 0;
+	char * file = "tiny_encoded.ppm";
+	struct ppm_pixel* arr = read_ppm(file, &w, &h);
+	printf("Testing file %s: %d %d\n", file, w, h);
+	for (int i = 0; i < w*h; i++) {
+		if (count < 8) {
+			unsigned char val = arr[i].red % 2;
+			str = str | val << (7 - count); 
+			count++;
+		}
+		if (count < 8) {
+			unsigned char val = arr[i].green % 2;
+                        str = str | val << (7 - count);  
+                        count++;
+		}	
+		if (count < 8) {
+			unsigned char val = arr[i].blue % 2;
+                        str = str | val << (7 - count);  
+			count++;
+		}
+		if (count == 8) {
+			printf("%c", str);
+			count = 0;	
+			str = 0;	
+		}
+	}
+	printf("\n");
+	free(arr);
+	arr = NULL;
+	return 0;
 }
 
--- A06/encode.c
+++ A06/encode.c
@@ -1,7 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
+char* string_to_binary(char* s) {
+	if(s == NULL) return 0; /* no input string */
+	size_t len = strlen(s);
+	char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
+	binary[0] = '\0';
+	for(size_t i = 0; i < len; ++i) {
+		char ch = s[i];
+		for(int j = 7; j >= 0; --j){
+			if(ch & (1 << j)) {
+				strcat(binary,"1");
+			} else {
+				strcat(binary,"0");
+			}
+		}
+	}
+	return binary;
+}
+
 int main(int argc, char** argv) {
-  return 0;
+	if (argc < 2) {
+		printf("Need param");
+		return -1;
+	}
+
+	printf("%s", string_to_binary("lol"));
+
+	char* file = argv[1];
+	int w = 0;
+	int h = 0;
+	struct ppm_pixel* pxs = read_ppm(file, &w, &h);
+	printf("Reading %s with a width %d and a height %d\n", file, w, h);
+	for (int i = 0; i < w * h; i++) {
+		printf("%d", pxs[i].red % 2);
+		printf("%d", pxs[i].green % 2);
+		printf("%d", pxs[i].blue % 2);
+	}
+	printf("\n");
+	int c = 1;
+	for (int i = 0; i < w*h; i++) {
+		printf("(%d, %d, %d)", pxs[i].red, pxs[i].green, pxs[i].blue);
+		if(c == w) {
+			printf("\n");
+			c = 0;
+		}
+		c++;
+	}
+	printf("\n");
+	int count = 0;
+	int temp = 0;
+	for (int i = 0; i < strlen(string_to_binary("lol")); i++) {
+		if (i % 3 == 0) {
+			temp = string_to_binary("lol")[i] - '0' == pxs[count].red % 2;
+			printf("%d", temp);
+			if (temp == 0) {
+				printf("!");
+				pxs[count].red = 1;			
+			}	
+		} else if (i % 3 == 1) {
+			temp = string_to_binary("lol")[i] - '0' == pxs[count].green % 2;
+			printf("%d", temp);
+			if (temp == 0) {
+				printf("!");
+				pxs[count].green = 2;
+			}
+		} else { // i % 3 == 2
+			temp = string_to_binary("lol")[i] - '0' == pxs[count].blue % 2;
+			count++;
+			printf("%d", temp);
+			if (temp == 0) {
+				printf("!");
+				pxs[count].blue = 3;
+			}
+		}
+	}
+	printf("DONE\n");
+	int c2 = 1;
+	for (int i = 0; i < w*h; i++) {
+		printf("(%d, %d, %d)", pxs[i].red, pxs[i].green, pxs[i].blue);
+		if(c2 == w) {
+			printf("\n");
+			c2 = 0;
+		}
+		c2++;
+	}
+	//printf("\nR %u %u", pxs[3].red, pxs[3].red + 1);
+	return 0;
+
 }
 
