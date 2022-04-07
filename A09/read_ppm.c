537083ad48e326a668f7a99f4e8bbbef9f887e3f
Bailey Machiko Hirota <bhirota@goldengate.cs.brynmawr.edu>
2022-04-07 15:40:17 -0400

    update

--- A09/read_ppm.c
+++ A09/read_ppm.c
@@ -4,13 +4,47 @@
 #include "read_ppm.h"
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+        FILE *fp;
+        struct ppm_pixel* arr;
+        char current_word[200];
+        int tempw, temph;
 
-  // your code here
-  return NULL;
+        fp = fopen(filename,"rb");
+        if(fp == NULL) { // check if successful
+                perror("Error in opening file");
+                return NULL;
+        }
+
+        for (int i = 0; i < 2; i++) {
+                fgets(current_word, sizeof(current_word), fp);
+        }
+        sscanf(current_word, "%d %d\n", &tempw, &temph);
+        if (current_word[0] == '#') {
+                fgets(current_word, sizeof(current_word), fp);
+                sscanf(current_word, "%d %d\n", &tempw, &temph);
+        }
+        fgets(current_word, sizeof(current_word), fp);
+        *w = tempw;
+        *h = temph;
+
+        arr = malloc(sizeof(struct ppm_pixel) * *w * *h);
+        if (arr == NULL) {
+                printf("Error: malloc failed\n");
+                return NULL;
+        }
+
+        fread(arr, sizeof(struct ppm_pixel), *w * *h, fp);
+
+        fclose(fp);
+
+        return arr;
 }
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+        FILE *fp;
+        fp = fopen(filename, "wb");
 
-  // your code here
+        fwrite(pixels, sizeof(struct ppm_pixel), w * h, fp);
+        fclose(fp);
 }
 
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -4,34 +4,83 @@
 #include <time.h>
 #include <sys/time.h>
 #include "read_ppm.h"
+#include <math.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-  int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
-  float ymin = -1.12;
-  float ymax = 1.12;
-  int maxIterations = 1000;
+	srand(time(0));
+	int size = 480;
+	float xmin = -2.0;
+	float xmax = 0.47;
+	float ymin = -1.12;
+	float ymax = 1.12;
+	int maxIterations = 1000;
 
-  int opt;
-  while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
-    switch (opt) {
-      case 's': size = atoi(optarg); break;
-      case 'l': xmin = atof(optarg); break;
-      case 'r': xmax = atof(optarg); break;
-      case 't': ymax = atof(optarg); break;
-      case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> -b <ymin> -t <ymax>\n", argv[0]); break;
-    }
-  }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+	int opt;
+	while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
+		switch (opt) {
+			case 's': size = atoi(optarg); break;
+			case 'l': xmin = atof(optarg); break;
+			case 'r': xmax = atof(optarg); break;
+			case 't': ymax = atof(optarg); break;
+			case 'b': ymin = atof(optarg); break;
+			case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> -b <ymin> -t <ymax>\n", argv[0]); break;
+		}
+	}
+	printf("Generating mandelbrot with size %dx%d\n", size, size);
+	printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
+	printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+	struct ppm_pixel* palette = malloc(sizeof(struct ppm_pixel));
+	
+	if (palette == NULL) {
+		printf("Malloc error");
+		return -1;
+	}	
 
-  // todo: your work here
-  // generate pallet
-  srand(time(0));
+	int row = size;
+	int col = size;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			float xfrac = (float)i/size;
+			float yfrac = (float)j/size;
+			float x0 = xmin + xfrac * (xmax-xmin);
+			float y0 = ymin + yfrac * (ymax-ymin);
+			
+			float x,y = 0;
+			int iter = 0;
 
-  // compute image
+			while (iter < maxIterations && x*x + y*y < 2*2) {
+				int xtmp = x*x - y*y + x0;
+				y = 2*x*y + y0;
+				x = xtmp;
+				iter++;
+			}
+			if (iter < maxIterations) { 
+				palette[iter].red = rand() % 255;
+				palette[iter].blue = rand() % 255;
+				palette[iter].green = rand() % 255;
+			}
+			else {
+				palette[iter].red = 0;
+				palette[iter].blue = 0;
+				palette[iter].green = 0;
+			}
 
+		}
+	}
+
+
+	// todo: your work here
+	// generate pallet
+	/*char* write = "mandelbrot";
+	strcat(write, "-");
+	strcat(write, size);
+	strcat(write, time(0));
+	strcat(write, ".ppm");
+	printf("Writing file %s\n", write);*/
+	write_ppm("example.ppm", palette, row, col);
+	free(palette);
+	return 0;
+
+	// compute image
 }
