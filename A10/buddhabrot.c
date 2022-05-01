2f851fba1ae4381242ea1f7c8c2be7821aee032c
Bailey Machiko Hirota <bhirota@goldengate.cs.brynmawr.edu>
2022-04-30 20:25:12 -0400

    resubmission 1/2

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -4,34 +4,260 @@
 #include <assert.h>
 #include <time.h>
 #include <pthread.h>
+#include <math.h>
 #include "read_ppm.h"
+#include <sys/time.h>
+#include <string.h>
+
+struct Info {
+	struct ppm_pixel* base;
+	struct ppm_pixel* palette;
+	int size;
+	float xmin;
+	float xmax;
+	float ymin;
+	float ymax;
+	int maxIterations;
+	int beginR;
+	int endR;
+	int beginC;
+	int endC;
+	pthread_mutex_t* mutex;
+	int *membership;
+	int *counter;
+	int *maxCount;
+	pthread_barrier_t* barrier;
+	int id;
+};
+
+void generate(struct Info* info) {
+	for (int i = info->beginR; i < info->endR; i++) {
+		for (int j = info->beginC; j < info->endC; j++) {
+			float xfrac = ((float)j)/info->size;
+			float yfrac = ((float)i)/info->size;
+			float x0 = info->xmin + xfrac * (info->xmax-info->xmin);
+			float y0 = info->ymin + yfrac * (info->ymax-info->ymin);
+
+			float x =  0;
+			float y = 0;
+			int iter = 0;
+
+			while (iter < info->maxIterations && (x*x + y*y) < 2*2) {
+				float xtmp = x*x - y*y + x0;
+				y = 2*x*y + y0;
+				x = xtmp;
+				iter++;
+			}
+
+			pthread_mutex_lock(info->mutex);
+			if (iter < info->maxIterations) { // escaped
+				info->membership[(i*(info->size))+j] = 0;
+			} else {
+				info->membership[(i*(info->size))+j] = 1;
+			}
+
+			pthread_mutex_unlock(info->mutex);
+
+		}
+
+	}
+	printf("Thread %d finished generate\n", info->id);
+}
+
+void step_two(struct Info *info) {
+	for (int i = info->beginR; i < info->endR; i++) {
+		for (int j = info->beginC; j < info->endC; j++) {
+			if (info->membership[(i*info->size)+j]) {
+				float xfrac = (float) (j+1)/info->size;
+				float yfrac = (float) (i+1)/info->size;
+				float x0 = info->xmin + xfrac * (info->xmax - info->xmin);
+				float y0 = info->ymin + yfrac * (info->ymax - info->ymin);
+				float x = 0;
+				float y = 0;
+				float xtmp;
+				while (x*x + y*y < 2*2) {
+					xtmp = x*x - y*y + x0;
+					y = 2*x*y + y0;
+					x = xtmp;
+
+					int yrow = round(info->size * (y - info->ymin)/(info->ymax - info->ymin));
+					int xcol = round(info->size * (x - info->xmin)/(info->xmax - info->xmin));
+
+					if (yrow < 0 || yrow >= info->size) continue; 
+					if (xcol < 0 || xcol >= info->size) continue;
+
+					pthread_mutex_lock(info->mutex);
+					info->counter[(yrow*info->size)+xcol]++;
+					if (info->counter[(yrow*info->size)+xcol] > *(info->maxCount)) {
+						*(info->maxCount) = info->counter[(yrow*info->size)+xcol];
+					}
+					pthread_mutex_unlock(info->mutex);
+				}
+			}	
+		}
+	}
+	printf("Thread %d finished step 2\n", info->id);
+}
+
+void step_three(struct Info *info) {
+	float gamma = 0.681;
+	float factor = 1.0/gamma;
+	for (int i = info->beginC; i < info->endC; i++) {
+		for (int j = info->beginR; j < info->endR; j++) {
+			float value = 0;
+			if (info->counter[(j*info->size)+i] > 0) {
+				value = (float)log(info->counter[(j*info->size)+i]) / (float)log(*(info->maxCount));
+				value = (float) pow(value, factor);
+			}
+			struct ppm_pixel color;
+			color.red = value * 255;
+			color.green = value * 255;
+			color.blue = value * 255;
+
+			pthread_mutex_lock(info->mutex);
+			info->palette[(j*(info->size))+i] = color;
+			pthread_mutex_unlock(info->mutex);
+
+		}
+	}
+	printf("Thread %d finished step 3i\n", info->id);
+}
+
+void run (void* info) {
+	struct Info* i = (struct Info*) info;
+	generate(i);
+	step_two(i);
+
+	pthread_barrier_wait(i->barrier);
+	step_three(i);
+}
 
 int main(int argc, char* argv[]) {
-  int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
-  float ymin = -1.12;
-  float ymax = 1.12;
-  int maxIterations = 1000;
-  int numProcesses = 4;
-
-  int opt;
-  while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
-    switch (opt) {
-      case 's': size = atoi(optarg); break;
-      case 'l': xmin = atof(optarg); break;
-      case 'r': xmax = atof(optarg); break;
-      case 't': ymax = atof(optarg); break;
-      case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
-    }
+	int size = 480;
+	float xmin = -2.0;
+	float xmax = 0.47;
+	float ymin = -1.12;
+	float ymax = 1.12;
+	int maxIterations = 1000;
+	int numProcesses = 4;
+	pthread_t* threads = malloc(sizeof(pthread_t) * numProcesses);
+  if (threads == NULL) {
+    printf("Malloc failed\n");
+    return -1;
   }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+  memset(threads, 0, sizeof(pthread_t) * numProcesses);
 
-  // todo: your code here
-  // compute image
+	int opt;
+	while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
+		switch (opt) {
+			case 's': size = atoi(optarg); break;
+			case 'l': xmin = atof(optarg); break;
+			case 'r': xmax = atof(optarg); break;
+			case 't': ymax = atof(optarg); break;
+			case 'b': ymin = atof(optarg); break;
+			case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
+						  "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+		}
+	}
+	printf("Generating mandelbrot with size %dx%d\n", size, size);
+	printf("  Num processes = %d\n", numProcesses);
+	printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
+	printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+
+	struct timeval tstart, tend;
+	double timer;
+	srand(time(0));
+	gettimeofday(&tstart, NULL);
+
+	struct ppm_pixel* base = (struct ppm_pixel*)malloc(maxIterations * sizeof(struct ppm_pixel));
+	struct ppm_pixel* palette = (struct ppm_pixel*)malloc(size * size * sizeof(struct ppm_pixel));
+
+	if (base == NULL) {
+		printf("Malloc error");
+		return -1;
+	}
+
+	if (palette == NULL) {
+		printf("Malloc error");
+		return -1;
+	}
+
+	for (int i = 0; i < maxIterations; i++) {
+		base[i].red = rand() % 255;
+		base[i].green = rand() % 255;
+		base[i].blue = rand() % 255;
+	}
+
+	memset(palette, 0, maxIterations * sizeof(struct ppm_pixel));
+
+	pthread_mutex_t mutex;
+	pthread_mutex_init(&mutex, NULL);
+
+	int beginR = 0;
+	int endR = size/sqrt(numProcesses);
+	int beginC = 0;
+	int endC = size/sqrt(numProcesses);
+
+	pthread_barrier_t barrier;
+	pthread_barrier_init(&barrier, NULL, numProcesses);
+	int* membership = malloc(sizeof(int)*size*size);
+	if (membership == NULL) {
+		printf("Malloc error");
+		return -1;
+	}
+	
+	memset(membership, 0, sizeof(int)*size*size);
+
+	int* maxCount = malloc(sizeof(int));
+	*maxCount = 0;;
+	int* counter = malloc(sizeof(int)*size*size);
+	if (counter == NULL) {
+		printf("Malloc failed");
+		return -1;
+	};
+	
+	memset(counter, 0, sizeof(int)*size*size);
+
+	struct Info* data = malloc(sizeof(struct Info)*numProcesses);
+	memset(data, 0, sizeof(struct Info)*numProcesses);
+
+	for (int i = 0; i < numProcesses; i++) {
+		int id = i+1;
+
+		struct Info info_chunk = {base, palette, size, xmin, xmax, ymin, ymax, maxIterations, beginR, endR, beginC, endC, &mutex, membership, counter, maxCount, &barrier, id};
+		data[i] = info_chunk;
+		printf("Thread %d) Sub-image block: cols (%d, %d) to rows (%d,%d)\n", i+1, \
+				beginR, endR, beginC, endC);
+		pthread_create(&threads[i], NULL, (void*) run, (void*) &data[i]);
+		if (i == sqrt(numProcesses)-1) {
+			beginR+= size/sqrt(numProcesses);
+			endR += size/sqrt(numProcesses);
+			beginC = 0;
+			endC = size/sqrt(numProcesses);
+		} else {
+			beginC += size/sqrt(numProcesses);
+			endC += size/sqrt(numProcesses);
+		}
+	}
+
+	for (int i = 0; i < numProcesses; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	pthread_mutex_destroy(&mutex);
+	pthread_barrier_destroy(&barrier);
+
+	gettimeofday(&tend, NULL);
+        timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+        printf("Computed buddhabrot set (%dx%d) in %.6g seconds\n", size, size,timer);
+        char filename[45];
+        sprintf(filename, "buddhabrot-%d-%ld.ppm", size, time(0));
+        printf("Writing file: %s\n", filename);
+        write_ppm(filename, palette, size, size);
+        free(palette);
+        free(base);
+	free(membership);
+	free(data);
+	free(maxCount);
+	free(threads);
+	free(counter);
 }
--- A10/thread_mandelbrot.c
+++ A10/thread_mandelbrot.c
@@ -11,6 +11,7 @@
 #include "read_ppm.h"
 
 struct Info {
+	int id;
 	struct ppm_pixel* base;
 	struct ppm_pixel* palette;
 	int size;
@@ -20,57 +21,60 @@ struct Info {
 	float ymax;
 	int maxIterations;
 	int beginR;
+	int endR;
 	int beginC;
 	int endC;
 	pthread_mutex_t* mutex;
 };
 
-void generate(struct ppm_pixel* base, struct ppm_pixel* palette, int size, float xmin, float xmax, float ymin, float ymax, int maxIterations, int beginR, int beginC, int endC, pthread_mutex_t* mutex) {
+void generate(int id, struct ppm_pixel* base, struct ppm_pixel* palette, int size, float xmin, float xmax, float ymin, float ymax, int maxIterations, int beginR, int endR, int beginC, int endC, pthread_mutex_t* mutex) {
 	float row = beginR;
 	float col = beginC;
-	for (int i = 0; i < size*size; i++) {
-		float xfrac = ((float)col)/size;
-		float yfrac = ((float)row)/size;
-		float x0 = xmin + xfrac * (xmax-xmin);
-		float y0 = ymin + yfrac * (ymax-ymin);
+	for (int r = beginR; r < endR; r++) {
+		for (int c = beginC; c < endC; c++) {
+			int i = size*r + c;
+			float xfrac = ((float)c)/size;
+			float yfrac = ((float)r)/size;
+			float x0 = xmin + xfrac * (xmax-xmin);
+			float y0 = ymin + yfrac * (ymax-ymin);
 
-		float x =  0;
-		float y = 0;
-		int iter = 0;
+			float x =  0;
+			float y = 0;
+			int iter = 0;
 
-		while (iter < maxIterations && (x*x + y*y) < 2*2) {
-			float xtmp = x*x - y*y + x0;
-			y = 2*x*y + y0;
-			x = xtmp;
-			iter++;
-		}
-		if (iter < maxIterations) {
-			pthread_mutex_lock(mutex);
-			palette[i].red = base[iter].red + rand() % 100 - 50;
-			palette[i].blue = base[iter].blue + rand() % 100 - 50;
-			palette[i].green = base[iter].green + rand() % 100 - 50;
-			pthread_mutex_unlock(mutex);
-		}
-		else {
-			palette[i].red = 0;
-			palette[i].blue = 0;
-			palette[i].green = 0;
-		}
-		col ++;
+			while (iter < maxIterations && (x*x + y*y) < 2*2) {
+				float xtmp = x*x - y*y + x0;
+				y = 2*x*y + y0;
+				x = xtmp;
+				iter++;
+			}
+			if (iter < maxIterations) {
+				pthread_mutex_lock(mutex);
+				palette[i].red = base[iter].red + rand() % 100 - 50;
+				palette[i].blue = base[iter].blue + rand() % 100 - 50;
+				palette[i].green = base[iter].green + rand() % 100 - 50;
+				pthread_mutex_unlock(mutex);
+			}
+			else {
+				palette[i].red = 0;
+				palette[i].blue = 0;
+				palette[i].green = 0;
+			}
+			col ++;
 
-		if (col == endC) {
-			row++;
-			col = 0;
-		}
+			if (col == endC) {
+				row++;
+				col = 0;
+			}
 
+		}
 	}
-	printf("Thread finished\n");
+	printf("Thread %d) finished\n", id);
 }
 
 void Compute(void *id) {
-        struct Info *myid = (struct Info *) id;
-	printf("compute\n");        
-        generate(myid->base, myid->palette, myid->size, myid->xmin, myid->xmax, myid->ymin, myid->ymax, myid->maxIterations, myid->beginR, myid->beginC, myid->endC, myid->mutex);
+	struct Info *myid = (struct Info *) id;
+	generate(myid->id, myid->base, myid->palette, myid->size, myid->xmin, myid->xmax, myid->ymin, myid->ymax, myid->maxIterations, myid->beginR, myid->endR, myid->beginC, myid->endC, myid->mutex);
 
 }
 
@@ -117,7 +121,7 @@ int main(int argc, char* argv[]) {
 		printf("Malloc error");
 		return -1;
 	}
-	
+
 	if (palette == NULL) {
 		printf("Malloc error");
 		return -1;
@@ -131,26 +135,30 @@ int main(int argc, char* argv[]) {
 
 	pthread_mutex_t mutex;
 	pthread_mutex_init(&mutex, NULL);
-	struct Info one = {base, palette, size, xmin, xmax, ymin, ymax, maxIterations, 0, 0, size/2, &mutex};
+	struct Info one = {1, base, palette, size, xmin, xmax, ymin, ymax, maxIterations, 0, size/2, 0, size/2, &mutex};
 	pthread_create(&thread1, NULL, (void*) Compute, (void*) &one);
-	struct Info two = {base, palette, size, xmin, xmax, ymin, ymax, maxIterations, size/2, 0, size/2, &mutex};
-        pthread_create(&thread2, NULL, (void*) Compute, (void*) &two);
-	struct Info three = {base, palette, size, xmin, xmax, ymin, ymax, maxIterations, 0, size/2, size, &mutex};
-        pthread_create(&thread3, NULL, (void*) Compute, (void*) &three);
-	struct Info four = {base, palette, size, xmin, xmax, ymin, ymax, maxIterations, size/2, size/2, size, &mutex};
-        pthread_create(&thread4, NULL, (void*) Compute, (void*) &four);
-	
+	printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d,%d)\n", one.id, 0, size/2, 0, size/2);
+	struct Info two = {2, base, palette, size, xmin, xmax, ymin, ymax, maxIterations, size/2, size, 0, size/2, &mutex};
+	pthread_create(&thread2, NULL, (void*) Compute, (void*) &two);
+	printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d,%d)\n", two.id, size, size, 0, size/2);
+	struct Info three = {3, base, palette, size, xmin, xmax, ymin, ymax, maxIterations, 0, size/2, size/2, size, &mutex};
+	pthread_create(&thread3, NULL, (void*) Compute, (void*) &three);
+	printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d,%d)\n", three.id, 0, size/2, size/2, size);
+	struct Info four = {4, base, palette, size, xmin, xmax, ymin, ymax, maxIterations, size/2, size, size/2, size, &mutex};
+	pthread_create(&thread4, NULL, (void*) Compute, (void*) &four);
+	printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d,%d)\n", four.id, size/2, size, size/2, size);
+
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
 	pthread_join(thread3, NULL);
 	pthread_join(thread4, NULL);
 
 	gettimeofday(&tend, NULL);
-        timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+	timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
 	printf("Computed mandelbrot set (%dx%d) in %.6g seconds\n", size, size,timer);
 	char filename[45];
 	sprintf(filename, "mandelbrot-%d-%ld.ppm", size, time(0));
-        printf("Writing file: %s\n", filename);
+	printf("Writing file: %s\n", filename);
 	write_ppm(filename, palette, size, size);
 	free(palette);
 	free(base);
